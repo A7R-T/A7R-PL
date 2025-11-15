00472feaf937f30ec56c8369d372c6245af2fd12
A7R <r7omany@gmail.com>
2025-11-15 07:22:42 +0200

    python OOP essentials self parameters

--- A7R-Python/OOP/essentials/OOP.py
+++ A7R-Python/OOP/essentials/OOP.py
@@ -1,14 +1,19 @@
 #the __init__ is used to intialize my class and I can put parameters there or not depending on my project needs
 #the parameter names has nothing to do with the property names that will get assigned to them later
 #self is just used to refer the the name of the object when it's created
+#any method/function (anything that required parentheses) should have self as the first parameter ALWAYS in any class
 class A7R:
     def __init__(self, the_name="Romany", the_skill="Programming", the_power="Infinite"):
         self.name = the_name
         self.skill = the_skill 
         self.power = the_power 
 
+    def greet(self):
+        print("Hello you almighty " + self.name)
+
 A7R_T = A7R("A7R")
 print (A7R_T.name)
 print (A7R_T.skill)
 print (A7R_T.power)
 
+A7R_T.greet()
