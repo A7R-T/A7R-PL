b1fd6ad804f4bcb7a7dbb1e02d7358c2464a53b2
A7R-T <r7omany@gmail.com>
2026-01-13 14:56:47 +0200

    first dart calculator :D

---
+++ A7R-Flutter/calculator/calc.dart
@@ -0,0 +1,37 @@
+import "dart:io";
+
+void main() {
+  print("==========================");
+  print("==========================");
+  print("==========================");
+  stdout.write('number1: ');
+  String? input1 = stdin.readLineSync();
+  stdout.write('operator: ');
+  String? opr = stdin.readLineSync();
+  stdout.write('number2: ');
+  String? input3 = stdin.readLineSync();
+
+  int Num1 = int.tryParse(input1 ?? '') ?? 0;
+  int Num2 = int.tryParse(input3 ?? '') ?? 0;
+
+  switch(opr) {
+  case ('+'):
+    stdout.write('The Result is: ');
+    print(Num1 + Num2);
+    break;
+  case ('-'):
+    print(Num1 - Num2);
+    break;
+  case ('*'):
+    print(Num1 * Num2);
+    break;
+  case ('/'):
+    print(Num1 / Num2);
+    break;
+  case ('%'):
+    print(Num1 % Num2);
+    break;
+  default:
+    print('Invalid Operation try again later');
+  }
+}
