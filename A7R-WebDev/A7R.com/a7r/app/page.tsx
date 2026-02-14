72545d735f9189df6280311d298e9cf9cfb92e9a
A7R-T <r7omany@gmail.com>
2026-02-15 01:44:14 +0200

    initializing my portofolio website with next.js and tailwind css

---
+++ A7R-WebDev/A7R.com/a7r/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex justify-center bg-[#061929]">
+      <h1 className="text-7xl font-bold text-center text-[#7cbcea] mt-10 p-7">A7R</h1>
+    </div>
+  );
+}
